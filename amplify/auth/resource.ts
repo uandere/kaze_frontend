08f4c8b476d99fde5a81bb21dce8bb9cda6c7103
Nazar Demchuk <nazar.demchvk@gmail.com>
2025-07-19 21:24:00 +0300

    Trying to add Google auth #1

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,23 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
-  },
+    externalProviders: {
+      google: {
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET')
+      },
+      callbackUrls: [
+        'http://localhost:3000/',
+        'https://aws-port.d3kdxwjozhszzs.amplifyapp.com',
+        'https://myrenta.org/'
+      ],
+      logoutUrls: [
+        'http://localhost:3000/',
+        'https://aws-port.d3kdxwjozhszzs.amplifyapp.com',
+        'https://myrenta.org/'
+      ]
+    }
+  }
 });
